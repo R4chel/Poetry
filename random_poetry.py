e8fe798c7c1e5da4c6db8eae8bd0757ad8df71e1
Rachel <rae84@cornell.edu>
2015-08-06 00:32:58 -0400

    read from local file
    also change probability of line break / end

--- random_poetry.py
+++ random_poetry.py
@@ -1,18 +1,16 @@
-from __future__ import division
 import nltk, random
 from nltk import word_tokenize
-import urllib
 import numpy as np
 
-url = "http://www.gutenberg.org/cache/epub/3021/pg3021.txt"
-response = urllib.urlopen(url)
-raw = response.read().decode('utf8')
+f = open('robertfrost500.txt')
+raw = f.read().decode('utf8')
 tokens = word_tokenize(raw)
 text = nltk.Text(tokens)
-tagged = nltk.pos_tag(text,tagset='universal')
-freqs = nltk.FreqDist(tagged)
+full_tagged = nltk.pos_tag(text,tagset='universal')
+tagged = [a for a in full_tagged if a[1] not in ['NUM','X','.']]
 
-tags = ('VERB','NOUN','PRON','ADJ','ADV','ADP','CONJ','DET','NUM','PRT','X','.')
+
+tags = ('VERB','NOUN','PRON','ADJ','ADV','ADP','CONJ','DET')
 probs = {}
 words = {}
 for tag in tags:
@@ -20,16 +18,21 @@ for tag in tags:
     nexts = [b[1] for (a, b) in bigrams if a[1] == tag]
     fdist = nltk.FreqDist(nexts)
     probs[tag] = [fdist.freq(t)/len(nexts) for t in tags]
-    words[tag] = [a[0] for a in tagged if a[1] == tag]
-tag_prob = [freqs.freq(tag) for tag in tags ]
+    words[tag] = list(set(a[0] for a in tagged if a[1] == tag))
 
-current_pos = np.random.choice(tags, 1, tag_prob)[0]
+freqs = nltk.FreqDist(tagged)
+start_prob = [freqs.freq(tag) for tag in tags]
+
+current_pos = np.random.choice(tags, 1, start_prob)[0]
 current_word = random.choice(words[current_pos])
 print(current_word),
-while random.randint(0, 20) <> 1:
-    current_pos = np.random.choice(tags, 1, probs[current_pos])[0]
-    current_word = random.choice(words[current_pos])
-    print current_word,
-    if random.randint(0, 10) <> 1:
-        print
-
+rand_lines = 0
+while rand_lines < 3:
+    rand_words = 0
+    while rand_words < 1:
+        current_pos = np.random.choice(tags, 1, probs[current_pos])[0]
+        current_word = random.choice(words[current_pos])
+        print(current_word),
+        rand_words += random.random()
+    rand_lines += random.random()
+    print
